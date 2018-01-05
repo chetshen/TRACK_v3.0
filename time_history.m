2c14befed5fc81cc9c2ebca1cb0edf030ee7e7cd
chenshen <chetshen@gmail.com>
2018-01-05 18:03:43 +0100

    Add comments: excluding inactive DOF from selected DOF

--- time_history.m
+++ time_history.m
@@ -1,17 +1,16 @@
- function [dis_out,vel_out,acc_out,index,nodeRef]=time_history(nodeNumber, dis, vel, acc, activeDof, dofID)
+function [dis_out,vel_out,acc_out,index,nodeRef]=time_history(nodeNumber, dis, vel, acc, activeDof, dofID)
 if nargin < 6
     dofID=1;
 end
 dof=2*(nodeNumber-1)+dofID;
 [index,loc]=ismember(activeDof,dof);
-loc1=loc(loc(:,1)~=0,1);
+loc1=loc(loc(:,1)~=0,1); %exclude those inactive DOF
 nodeRef=zeros(length(loc1),1);
 for i=1:length(loc1)
-nodeRef(i,1)=(dof(loc1(i))-dofID)/2+1;
+    nodeRef(i,1)=(dof(loc1(i))-dofID)/2+1; %calculate correspong nodes according to active DOF
 end
 
 dis_out=dis(:,index);
 vel_out=vel(:,index);
 acc_out=acc(:,index);
- end
- 
+end
