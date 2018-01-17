b0e21347bc3675f40be3616757c4531efc6359b7
chenshen <chetshen@gmail.com>
2018-01-17 10:55:47 +0100

    Minor: add output argument and frequency label as figure title

--- plt_eig.m
+++ plt_eig.m
@@ -1,4 +1,4 @@
-function []=plt_eig(V,geo,mat_trk,range,modenum,scale)
+function [NodeNumber, dofi, nodei,shape,Vshape]=plt_eig(V,geo,mat_trk,range,modenum,eigfrq,scale)
 [NodeNumber]=picknodes(range, geo.ND); %define the node numbers to be plotted
 [~,~,~,dofi,nodei]=time_history(NodeNumber,mat_trk.activeDof); %  get the reference index of the nodes to be plotted
 
@@ -9,4 +9,5 @@ shape=zeros(length(geo.ND),1);
 shape(nodei)=Vshape(:,modenum);
 plot_modeShape(geo,shape,scale); 
 xlim('Auto');
+ title([num2str(eigfrq(modenum)),' Hz'])
 end
