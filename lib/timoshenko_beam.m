b24343efce1ca49a9b7bab9c7106425ff65641dc
chenshen <chetshen@gmail.com>
2018-01-25 15:41:24 +0100

    MAJOR: New mass matrix for timoshenko beam element

--- lib/timoshenko_beam.m
+++ lib/timoshenko_beam.m
@@ -25,14 +25,22 @@ K=EI/(phi1*(LElem)^3)*[12         6*LElem       -12        6*LElem
                         -12       -6*LElem      12         -6*LElem
                        6*LElem    phi3*LElem^2  -6*LElem   phi2*LElem^2];
 %Mass matrix 
-% M=((Arho*LElem)/840)*[phim1          phim2*LElem     phim3          phim4*LElem
-%                       phim2*LElem    phim5*LElem^2   phim6*LElem    phim7*LElem^2
-%                       phim3          phim6*LElem     phim8          phim9*LElem
-%                       phim4*LElem    phim7*LElem^2   phim9*LElem    phim10*LElem^2];
-M=((Arho*LElem)/420)*[156         22*LElem    54          -13*LElem
-                      22*LElem    4*LElem^2   13*LElem    -3*LElem^2
-                      54          13*LElem    156         -22*LElem
-                      -13*LElem   -3*LElem^2  -22*LElem   4*LElem^2];
+Mt=((Arho*LElem)/(840*phi1^2))...
+    *[phim1          phim2*LElem     phim3          phim4*LElem
+      phim2*LElem    phim5*LElem^2   phim6*LElem    phim7*LElem^2
+      phim3          phim6*LElem     phim8          phim9*LElem
+      phim4*LElem    phim7*LElem^2   phim9*LElem     phim10*LElem^2];
+Mr=((rho*I)/(30*phi1^2)*LElem)...
+    *[36                 -(15*phi-3)*LElem           -36                 -(15*phi-3)*LElem
+    -(15*phi-3)*LElem   (10*phi^2+5*phi+4)*LElem^2  (15*phi-3)*LElem    (5*phi^2-5*phi-1)*LElem^2
+    -36                 (15*phi-3)*LElem             36                  (15*phi-3)*LElem
+    -(15*phi-3)*LElem   (5*phi^2-5*phi-1)*LElem^2    (15*phi-3)*LElem    (10*phi^2+5*phi+4)*LElem^2];
+M=Mt+Mr;
+
+% M=((Arho*LElem)/420)*[156         22*LElem    54          -13*LElem
+%                       22*LElem    4*LElem^2   13*LElem    -3*LElem^2
+%                       54          13*LElem    156         -22*LElem
+%                       -13*LElem   -3*LElem^2  -22*LElem   4*LElem^2];
 
 
 end
