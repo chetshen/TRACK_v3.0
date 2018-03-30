5fdeb9633e9f1df9093b7e2a710a244890aa7993
chenshen <chetshen@gmail.com>
2018-03-30 16:34:30 +0200

    cherry-pick 0f9a3c

--- input/get_input_2.m
+++ input/get_input_2.m
@@ -1,12 +1,12 @@
 %%
-%FE model by Maider
+%Parameter to match measurement in Maider's thesis 20180312
 %%
 function [in_data] = get_input_2(in_data)
 %%
 %Geometry input
 
-in_data.geo.Ltot_R=12;                  %[m]
-in_data.geo.SlpSpc=0.6;          %[m] 
+in_data.geo.Ltot_R=30;                  %[m]
+in_data.geo.SlpSpc=0.6;           %[m] 
 in_data.geo.dist_S=in_data.geo.SlpSpc; %[m] 
 in_data.geo.LExt_S=0.54;             %[m]
 in_data.geo.LInt_S=0.750;              %[m]
@@ -22,7 +22,7 @@ in_data.geo.irr=0;                     % Irregularities
 
 in_data.ext_force.timeh='example.txt';%'FW_h30w40'; %['white_noise.txt']; %[ 'example.txt' ];        %time history of external force
 in_data.ext_force.sf=25600;
-in_data.ext_force.x=[5.1,-0.75,0];
+in_data.ext_force.x=[15.3,-0.75,0];
 in_data.ext_force.Vx=0;
 % zdd=load(in_data.ext_force.timeh);
 % dof=299;
@@ -36,14 +36,27 @@ in_data.solver.n_ts=5000; %length(zdd)-1;                     %Number of time st
 in_data.solver.deltat=0.00004;                   %Time step length
 in_data.solver.linsolver_id=2;             %linear solver id, 1 for LDL, 2 for mldivide
 in_data.solver.Vx=30;
+%%
+%MESH PARAMETERS
+in_data.mesh.numElem_R_betwSprings=12;   %Number of elements between 2 springs
+% in_data.mesh.numElem_R_betwSprings_L=60;   %Number of elements between 2 springs
+in_data.mesh.RefinedMeshLength=0.001;    %Element length at refined mesh around irregularity [m]
+in_data.mesh.m_1S_Ext=4;                %Number of elements in a sleeper external
+in_data.mesh.m_1S_Int=12;                %Number of elements in a sleeper internal
+NEslph=(2*in_data.mesh.m_1S_Ext+in_data.mesh.m_1S_Int)/2; %number of elements for half sleeper
+NNslpf=NEslph*2+1;
+NNslph=NEslph+1;
+
+in_data.mesh.btypr=2;                   %mesh beam type: 1 for Euler, 2 for Timoshenko
+in_data.mesh.btyps=2;
 
 %%
 
 %MATERIAL RAIL DATA 
 % in_data.mater(1).ElemType=1;       %1 for rail material; 2 for sleeper; 3 for railpads; 4 for ballas2.951t
 in_data.mater(1).Data=[210e9;    %E_R[N/m^2]
-                    0.23896e-4; %2.417e-5;    %I_R[m^4]
-                   7.0515e-3; %7.246e-3;   %A_R[m^2]
+                    0.23896e-4;    %0.23896e-4; %2.417e-5;    %I_R[m^4]
+                    7.0515e-3;   %7.0515e-3; %7.246e-3;   %A_R[m^2]
                     7800;    %rho_R[kg/m^3]
                       210e9/2.6;    %G_R
                      0.833];% 0.34] ;     %kappa_R
@@ -51,11 +64,11 @@ in_data.mater(1).Note='rail';
 
 %MATERIAL SLEEPERS DATA 
 % in_data.mater(2).ElemType=3;
-in_data.mater(2).Data=[39e9; % #19.4e12# or #19.4e9# [N/m^2]
+in_data.mater(2).Data=[74.625e9; % #19.4e12# or #19.4e9# [N/m^2]
                     1.375e-4;  %[m^4]
                     0.043; %[m^2]
                     2500;%2140;%3070;   %2480[kg/m^3]
-                    39e9/2.34; %E/2.34
+                    74.6e9/2.34;%74.6e9/2.34; %E/2.34
                     0.833]; %0.833
 in_data.mater(2).Note='sleeper';
 
@@ -68,8 +81,8 @@ in_data.mater(3).Note='railpad';
 
 %MATERIAL SPRING DATA: BALLAST
 % in_data.mater(4).ElemType=4;
-in_data.mater(4).Data = [9e-7;  %K_Spring_SB[N/m]
-                         6.4e-7];%3.444e4/5];  %C_Damper_SB[N.s/m]
+in_data.mater(4).Data = [9e7/NNslpf;  %K_Spring_SB[N/m]
+                         6.4e4/NNslpf];%3.444e4/5];  %C_Damper_SB[N.s/m]
 in_data.mater(4).Note='ballast';
 
 
@@ -82,7 +95,7 @@ in_data.mater(5).Note='contact';
 in_data.mater(6).Data =  [0;   %M_sprg%4653.5[kg]
                       883.6;%883.6; %M_unsprg[kg]
                      1.22e6]; %K_PS[N/m]
-in_data.mater(6).wsfile='E:\FE\Model 1 wheel\half_ws.spm';  %.spm file generated from ANSYS; empty means a rigid wheelset               
+in_data.mater(6).wsfile='';%'E:\FE\Model 1 wheel\half_ws.spm';  %.spm file generated from ANSYS; empty means a rigid wheelset               
 in_data.mater(6).Note='vehicle';
                  
 %==================Add new materials below=================================
