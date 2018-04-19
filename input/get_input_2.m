208b0e77b1fee4bb569f052233dac64ba506821b
chenshen <chetshen@gmail.com>
2018-04-19 17:26:04 +0200

    Enhancement: hammer test- smaller sampling intervals due to period elongation

--- input/get_input_2.m
+++ input/get_input_2.m
@@ -21,8 +21,8 @@ in_data.geo.irr=0;                     % Irregularities
 %External force
 
 in_data.ext_force.timeh='example.txt';%'FW_h30w40'; %['white_noise.txt']; %[ 'example.txt' ];        %time history of external force
-in_data.ext_force.sf=25600;
-in_data.ext_force.x=[5.1,-0.75,0];
+in_data.ext_force.sf=102400;
+in_data.ext_force.x=[6.3,-0.75,0];
 in_data.ext_force.Vx=0;
 % zdd=load(in_data.ext_force.timeh);
 % dof=299;
@@ -38,7 +38,7 @@ in_data.solver.linsolver_id=2;             %linear solver id, 1 for LDL, 2 for m
 in_data.solver.Vx=30;
 %%
 %MESH PARAMETERS
-in_data.mesh.numElem_R_betwSprings=6;   %Number of elements between 2 springs
+in_data.mesh.numElem_R_betwSprings=24;   %Number of elements between 2 springs
 % in_data.mesh.numElem_R_betwSprings_L=60;   %Number of elements between 2 springs
 in_data.mesh.RefinedMeshLength=0.001;    %Element length at refined mesh around irregularity [m]
 in_data.mesh.m_1S_Ext=40;                %Number of elements in a sleeper external
