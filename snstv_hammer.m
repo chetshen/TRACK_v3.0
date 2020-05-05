41ae40297106fb886dffabd1d959b60467a216f4
chenshen <chetshen@gmail.com>
2020-05-06 00:35:25 +0200

    snst analysis with numerical example 2 (Zhang 2019)

--- snstv_hammer.m
+++ snstv_hammer.m
@@ -3,11 +3,11 @@
 % % Author: Chen Shen
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-inp = get_input_4();
-para = logspace(-1,1,10);%linspace(0.1,1.1,10);%para = [0.9,1,1.1];linspace(0.1,2,20);%[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8];
+inp = get_input_Zhang_2019();
+para = linspace(0.1,1.1,10);%logspace(-1,1,10);%linspace(0.1,1.1,10);%para = [0.9,1,1.1];linspace(0.1,2,20);%[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8];
 para = round(para,1);
 % para = [0.5 0.6 0.7 0.8 0.9 1 1.1 1.2 1.3];
-nmat = 4;
+nmat = 3;
 ndata = 1; 
 refval=inp.mater(nmat).Data(ndata);
 btypr=inp.mesh.btypr;
@@ -16,16 +16,17 @@ btyps=inp.mesh.btyps;
 S=struct('H1',zeros(10,1));
 % S1=struct('frq',zeros(10,1));
 
-w=genexpwin(1000);
+% w=genexpwin(1000);
 
-for i = 1:length(para)
-   inp.mater(nmat).Data(ndata)= para(i)*refval;
+parfor i = 1:length(para)
+   inpOne = inp;
+   inpOne.mater(nmat).Data(ndata)= para(i)*refval;
 [geo] = mesh_trk_full(btypr,btyps,nodeCoord);
-mat_trk = form_mat_trk_2(inp,geo);
+mat_trk = form_mat_trk_2(inpOne,geo);
 
-hammer_main;
+H1 = hammer_main_snst_mcs(inpOne,mat_trk,geo);
 
-[H1,H2,~,pxx,pxy,fxx]=tran_fun([force(1:1000)',dis_x_load(1:1000)],w,0,25600,25000);
+% [H1,H2,~,pxx,pxy,fxx]=tran_fun([force(1:1000)',dis_x_load(1:1000)],w,0,25600,25000);
 S(i).H1=H1;
 
 end
