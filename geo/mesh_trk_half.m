b21b12e5ce423a5f61fc2b1f8bc32e1d7187c891
chenshen <chetshen@gmail.com>
2020-08-11 16:25:22 +0200

    Minor: include anti-symmetric bc in annotations

--- geo/mesh_trk_half.m
+++ geo/mesh_trk_half.m
@@ -82,8 +82,9 @@ LeftRailNodes=geo.ND(geo.ND(:,5)==1,1);
 SleeperNodes(:,1)=geo.ND(geo.ND(:,3)==0 & geo.ND(:,5)==3,1);%geo.ND(geo.ND(:,5)==3,1);%geo.ND(geo.ND(:,3)==0 & geo.ND(:,5)==3,1);
 %%%%%%%%%%%%%can be disabled if necessary
 
-geo.fixedNodeU=[LeftRailNodes(1);LeftRailNodes(length(LeftRailNodes));ballastNodes(:,1)];
+%geo.fixedNodeU=[LeftRailNodes(1);LeftRailNodes(length(LeftRailNodes));SleeperNodes(:,1);ballastNodes(:,1)];
 
+geo.fixedNodeU=[LeftRailNodes(1);LeftRailNodes(length(LeftRailNodes));ballastNodes(:,1)];
 %%%%%%%%%%%%%%%
 geo.fixedNodeV=[LeftRailNodes(1);LeftRailNodes(length(LeftRailNodes));SleeperNodes(:,1);ballastNodes(:,1)];
 % geo.fixedNodeV=[LeftRailNodes(1);LeftRailNodes(length(LeftRailNodes));ballastNodes(:,1)];
