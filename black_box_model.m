3f6c17557055aa31e0c05cf23b49eaa6579a4a9c
Chen Shen <chenshen@tudelft.nl>
2022-11-16 23:44:49 +0100

    removed input cp, cb; added input number of sleeper elements

--- black_box_model.m
+++ black_box_model.m
@@ -1,11 +1,12 @@
-function [H1, fxx] = black_box_model(kp,cp,kb,cb)
+function [H1, fxx] = black_box_model(kp,kb,num_elem_slp)
 %%
 % black_box_model: calculates FRF based on hammer test
 % INPUT:
 %   kp - railpad stiffness
-%   cp - railpad damping
+%   
 %   kb - ballast stiffness
-%   cb - ballast damping
+%   num_elem_slp - number of elements in one sleeper, should be an
+%   integral and greater than 1
 % OUTPUT:
 % 	H1 - FRF (point receptance, complex value)
 %  fxx - frequency points correponding to FRF
@@ -13,9 +14,15 @@ function [H1, fxx] = black_box_model(kp,cp,kb,cb)
 % see example_black_box_optimization.mlx
 % 
 
+
+
 % get input parameters
 [inp,NNslpf] = get_input_4(); % default input parameters
-
+if nargin == 3
+inp.mesh.m_1S_Int = num_elem_slp;
+NEslph=(2*inp.mesh.m_1S_Ext+inp.mesh.m_1S_Int)/2; %number of elements for half sleeper
+NNslpf=NEslph*2+1;
+end
 % geometry
 [nodeCoord] = node_coor(inp); % form nodes
 btypr = inp.mesh.btypr;       % element type for rail
@@ -23,10 +30,11 @@ btyps = inp.mesh.btyps;       % element type for sleeper
 [geo] = mesh_trk_full(btypr,btyps,nodeCoord); % mesh geometry
 
 % modify railpad and ballast stiffness and damping (parameters to be optimized)
+
 inp.mater(3).Data(1) = kp;
-inp.mater(3).Data(2) = cp;
+% inp.mater(3).Data(2) = cp;
 inp.mater(4).Data(1) = kb / NNslpf;
-inp.mater(4).Data(2) = cb / NNslpf;
+% inp.mater(4).Data(2) = cb / NNslpf;
 
 % form system matrix
 % mat_ws=form_mat_ws(inp);                      % wheelset matrix
